8f082bef17ebd1448c854a559e692aaa12965247
Danny <danny05261@gmail.com>
2018-07-08 01:11:13 +0800

    simple-read-display-csv: complete

--- simple-read-display-csv/src/ReadFileWriteToConsole.java
+++ simple-read-display-csv/src/ReadFileWriteToConsole.java
@@ -1,5 +1,7 @@
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import com.csvreader.CsvReader;
 
 public class ReadFileWriteToConsole {
@@ -7,26 +9,44 @@ public class ReadFileWriteToConsole {
 	private static final int MAXROW = 3;
 	private static final int MAXCOL = 3;
     private static final String inFile = "in.csv";
-	   
     public String[][] sheet = new String[MAXROW][MAXCOL];
 	   
     public static void main(String args[]) throws IOException {
     	// create a ReadWriteToConsole object
     	//  Do not change the signature of this method.
-    	// ... insert code here ...
+    	ReadFileWriteToConsole console = new ReadFileWriteToConsole();
     	// invoke readSheet()
-    	// ... insert code here ...
+    	console.readSheet();
     	// invoke writeSheet()
-    	// ... insert code here ...
+    	console.writeSheet();
     }	
 	   
     public void readSheet() throws IOException {
-    	// ... insert code here ...
+        InputStream stream = new FileInputStream(inFile);
+        CsvReader csvReader = new CsvReader(stream, StandardCharsets.UTF_8);
+        
+        int row = 0;
+        while(csvReader.readRecord() && row < MAXROW){
+            String[] values = csvReader.getValues();
+            for(int col= 0; col < values.length; col++){
+            	this.sheet[row][col] = values[col];
+            }
+            row++;
+        }
+        csvReader.close();
     	//  Do not change the signature of this method.
 	}
 	   
 	public void writeSheet(){
-		// ... insert code here ...
+        for(int row=0; row < MAXROW; row++){
+            for(int col=0; col < MAXCOL; col++){
+                String cell = this.sheet[row][col];
+                if(cell != null){
+                    System.out.print("["+cell+"]");
+                }
+            }
+            System.out.println();
+        }
     	//  Do not change the signature of this method.
 	}
 }
